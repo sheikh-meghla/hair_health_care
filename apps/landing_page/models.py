c0505ec6705a6d8a1665a55db25f4e9bbd3e3f76
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-28 15:03:31 +0600

    Install python pillow

--- apps/landing_page/models.py
+++ apps/landing_page/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class FooterSection(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+class HowItWorks(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
