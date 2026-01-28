16795ed33bfbb2ca615bf9803db117a015d2075a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-28 14:38:49 +0600

    add REST framework with JWT authentication settings

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'apps.user',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
